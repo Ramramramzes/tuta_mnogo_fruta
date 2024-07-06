822df440271acee14abec2e7d5c543ba07ea0553
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-07-06 21:36:18 +0300

    background carousel animation

--- src/components/CarouselGeneral/CarouselGeneral.tsx
+++ src/components/CarouselGeneral/CarouselGeneral.tsx
@@ -11,15 +11,15 @@ export function CarouselGeneral() {
     <Carousel indicators={false}>
       {slides.slider.map((el,index) => {
         return(
-          <Carousel.Item key={index} className={styles.caruselItem} style={{backgroundImage: `url(${el.image})`}}>
-            <div className={styles.filter}></div>
+          <Carousel.Item key={index} className={styles.caruselItem}> 
+            <img src={el.image} className={styles.img}/>
             <Carousel.Caption className={styles.carouselCaption} style={index === 0 ? {textAlign: 'left'} : index === 1 ? {textAlign: 'right',alignItems: 'right'} : {textAlign: 'center'}}>
-              <h3 className={styles.caruselTitle}>{el.title}</h3>
-              <p className={styles.caruselDescription}>{el.description}</p>
-              <div className={styles.btnBlock} style={index === 0 ? {justifyContent: 'left'} : index === 1 ? {justifyContent: 'right'} : {}}>
-                {index != 2 ? <Link className={'link ' + styles.btn} to={'/catalog'}>Подробнее</Link> : ''}
-              </div>
-            </Carousel.Caption>
+                <h3 className={styles.caruselTitle}>{el.title}</h3>
+                <p className={styles.caruselDescription}>{el.description}</p>
+                <div className={styles.btnBlock} style={index === 0 ? {justifyContent: 'left'} : index === 1 ? {justifyContent: 'right'} : {}}>
+                  {index != 2 ? <Link className={'link ' + styles.btn} to={'/catalog'}>Подробнее</Link> : ''}
+                </div>
+              </Carousel.Caption>
           </Carousel.Item>
         )
       })}
