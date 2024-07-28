96476bb9069e4991477603ff072ff6dce55ca95c
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-07-28 18:41:54 +0300

    animation fix & item page loading add

--- src/components/ItemElement/ItemElement.tsx
+++ src/components/ItemElement/ItemElement.tsx
@@ -22,36 +22,34 @@ export function ItemElement() {
     }
   },[CatalogState.allProducts])
 
-
-  
-
-  
-
-  
-
   return (
     <>
-      <div className={styles.pageContent + ' container'}>
-        {itemData && <>
-          <div className={styles.mainPart}>
-            <ItemImage itemData={itemData} />
-            <ItemDescription />
-          </div>
-          <div className={styles.descriptionBlock}>
-            <h3 className={styles.descriptionTitle}>Описание:</h3>
-            {ItemState.currentItem.description && 
-              <>
-                <p className={styles.description}>{ItemState.currentItem.description.replace(/<[^>]*>/g, '')}</p>
-                <p><span className={styles.srokTitle}>Срок хранения:</span>{ItemState.currentItem.meta_data.filter((el:{id: number,key:string,value:string}) => el.key ==='srok')[0].value}</p>
-              </>
-            }
-            <h3 className={styles.same}>Похожие товары:</h3>
-          </div>
-        </>}
-      </div>
-      <div className={styles.bestBlock}>
-        <Bestsellers products={sameProducts} desktop={4} height={70} />
-      </div>
+    {!itemData.loading ? 
+      <>
+        <div className={styles.pageContent + ' container'}>
+          {itemData && <>
+            <div className={styles.mainPart}>
+              <ItemImage itemData={itemData} />
+              <ItemDescription />
+            </div>
+            <div className={styles.descriptionBlock}>
+              <h3 className={styles.descriptionTitle}>Описание:</h3>
+              {ItemState.currentItem.description && 
+                <>
+                  <p className={styles.description}>{ItemState.currentItem.description.replace(/<[^>]*>/g, '')}</p>
+                  <p><span className={styles.srokTitle}>Срок хранения:</span>{ItemState.currentItem.meta_data.filter((el:{id: number,key:string,value:string}) => el.key ==='srok')[0].value}</p>
+                </>
+              }
+              <h3 className={styles.same}>Похожие товары:</h3>
+            </div>
+          </>}
+        </div>
+        <div className={styles.bestBlock}>
+          <Bestsellers products={sameProducts} desktop={4} height={70} />
+        </div>
+      </>
+      :
+      <div className={styles.loaderBlock + ' loader-block'}><span className='loader'></span></div>}
     </>
   );
 }
