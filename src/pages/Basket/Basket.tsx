ed5e03a268068db58237c3aac125cb3d0d1aff72
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-08-28 17:52:40 +0300

    fix: изменения в странице корзины

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -1,14 +1,24 @@
-import { useNavigate } from 'react-router-dom';
+import styles from './basket.module.scss'
+import { useSelector } from 'react-redux';
 import { LayoutBase } from '../../layout/LayoutBase';
+import { RootState } from '../../store/store';
+import { BasketItem } from '../../components/BasketItem';
 
 export function Basket() {
-  const navigate = useNavigate()
-  const navHandler = (link: string) => {
-    navigate(link)
-  }
+  const BasketState = useSelector((state: RootState) => state.basket);
+  console.log(BasketState);
+  
   return (
     <LayoutBase>
-      <button onClick={() => navHandler('/')}>back</button>
+      {BasketState.length === 0 ?
+        <span className={styles.empty}>Корзина пуста :(</span>
+      :
+        <div className={styles.itemBlock + ' container'}>
+          {BasketState.map((el,index) => (
+            <BasketItem key={index} el={el} />
+          ))}
+        </div>
+      }
     </LayoutBase>
   );
 }
