3030b6ba71744cc835ceb311d74c14879eca3179
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-08-31 21:24:06 +0300

    fix:  исправил логику работы корзины

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -2,15 +2,47 @@ import styles from './basket.module.scss'
 import { useSelector } from 'react-redux';
 import { LayoutBase } from '../../layout/LayoutBase';
 import { RootState } from '../../store/store';
-import { BasketItem } from '../../components/BasketItem';
+// import { BasketItem } from '../../components/BasketItem';
 import { UseBasketList } from '../../hooks/useBasketList';
+import { useEffect, useState } from 'react';
 
 
 export function Basket() {
   const BasketState = useSelector((state: RootState) => state.basket);
-  const { item, loading, error } = UseBasketList()
+  const { item } = UseBasketList()
+  // const { item, loading, error } = UseBasketList()
 
-  console.log(item,loading,error);
+  const [finalBasket, setFinalBasket] = useState<IFinalBasket[]>([])
+
+  interface IFinalBasket{
+    name?: string;
+    price?: number;
+    size?: string;
+    quantity?: number;
+  }
+
+  useEffect(() => {
+    const updatedBasket: IFinalBasket[] = [];
+  
+    BasketState.forEach(el => {
+      const found = item.filter(item => item.product.id === el.id);
+      const needVariation = found[0]?.variation.find(item => item.name === el.size);
+  
+      const readyToPush: IFinalBasket = {
+        name: found[0]?.product.name,
+        price: needVariation?.price,
+        size: needVariation?.name,
+        quantity: el.quantity
+      };
+  
+      updatedBasket.push(readyToPush);
+    });
+  
+    setFinalBasket(updatedBasket);
+  
+  }, [BasketState, item]);
+  
+  console.log(finalBasket);
   
   return (
     <LayoutBase>
@@ -18,9 +50,9 @@ export function Basket() {
         <span className={styles.empty}>Корзина пуста :(</span>
       :
         <div className={styles.itemBlock + ' container'}>
-          {BasketState.map((el,index) => (
+          {/* {BasketState.map((el,index) => (
             <BasketItem key={index} el={el} />
-          ))}
+          ))} */}
         </div>
       }
     </LayoutBase>
